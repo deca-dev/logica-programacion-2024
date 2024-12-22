8cd417deab043b251a63dadfd71e551fb4225147
David Castro <davidernesto.castroanaya@gmail.com>
2024-12-22 12:22:21 -0600

    Adding exercise 00

---
+++ 00 - sintaxis, variables, tipos de datos, hola mundi/index.js
@@ -0,0 +1,98 @@
+/*
+- Add a comment in the code and include the URL of the official website for
+the programming language you have chosen.
+- Demonstrate the different syntax options for creating comments in the language
+(single-line, multi-line, etc.).
+- Create a variable (and a constant if the language supports it).
+- Create variables representing all primitive data types of the language
+(strings, integers, booleans, etc.).
+- Print to the terminal the text: "Hello, [and the name of your language]!"
+*/
+
+// =======================================
+
+/*
+
+URL Oficial of JS:
+
+https://developer.mozilla.org/
+
+*/
+
+// This is an single line comment
+
+/*
+This is a multi-line
+comment 
+*/
+
+// =========================================
+
+/*
+
+Variables:
+
+- Creating a variable:
+
+    We use the keyword: 'let''
+
+    let myName;
+    let myAge;
+
+- Initializing the Variable:
+
+    myName = "David";
+    myAge = 25;    
+
+- Declaring and Initializing:
+
+    let myName = "David";
+    let myAge = 25;  
+
+
+*/
+
+//  ====================================
+
+/*
+Variable Types:
+
+Numbers, Strings, Booleans, Arrays, Objects
+
+ - Numbers:
+    let myAge = 17;
+
+ - Strings:
+    let dolphinGoodbye = "So long and thanks for all the fish";
+
+ - Booleans:
+    let iAmAlive = true;
+    let test = 6 < 3;
+
+ - Arrays:
+    let myNameArray = ["Chris", "Bob", "Jim"];
+    let myNumberArray = [10, 15, 40];
+        myNameArray[0]; // should return 'Chris'
+        myNumberArray[2]; // should return 40
+
+ - Objects:
+    let dog = { name: "Spot", breed: "Dalmatian" };
+
+*/
+
+// =============================================
+
+/*
+
+Constants:
+
+const count = 1;
+
+const bird = { species: "Kestrel" };
+console.log(bird.species); // "Kestrel"
+    bird.species = "Striated Caracara";
+console.log(bird.species); // "Striated Caracara"
+
+*/
+
+console.log("Hello, JS!");
