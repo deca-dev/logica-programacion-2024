9229a12c9d9185d825ddfb69ef3c662350a99dac
David Castro <davidernesto.castroanaya@gmail.com>
2025-01-10 10:52:59 -0600

    Completing diagonal differnece

--- extras/easy/04-diagonal-difference.js
+++ extras/easy/04-diagonal-difference.js
@@ -6,11 +6,11 @@ For example, the square matrix  is shown below:
 1 2 3
 4 5 6
 9 8 9  
-The left-to-right diagonal (1,5,9) =  . The right to left diagonal = . Their absolute difference is .
+The left-to-right diagonal (1 +5 +9) = 15  . The right to left diagonal = 3 + 5 + 9 = 17 . Their absolute difference is \15-17\ =2 .
 
 Function description
 
-Complete the  function in the editor below.
+Complete the diagnolaDifference  function in the editor below.
 
 diagonalDifference takes the following parameter:
 
@@ -18,10 +18,11 @@ int arr[n][m]: an array of integers
 Return
 
 int: the absolute diagonal difference
+
 Input Format
 
-The first line contains a single integer, , the number of rows and columns in the square matrix .
-Each of the next  lines describes a row, , and consists of  space-separated integers .
+The first line contains a single integer, n, the number of rows and columns in the square matrix arr.
+Each of the next n  lines describes a row, arr[i] , and consists of n space-separated integers arr[i][j].
 
 Constraints
 
@@ -35,9 +36,10 @@ Sample Input
 11 2 4
 4 5 6
 10 8 -12
-Sample Output
 
+Sample Output
 15
+
 */
 
 let arr = [ 
