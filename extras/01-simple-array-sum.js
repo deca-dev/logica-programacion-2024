72ab51ce2276bed29c5a463602264709d467adba
David Castro <davidernesto.castroanaya@gmail.com>
2025-01-09 20:23:37 -0600

    Adding 01

--- extras/01-simple-array-sum.js
+++ extras/01-simple-array-sum.js
@@ -1,7 +1,7 @@
 /*
 Given an array of integers, find the sum of its elements.
 
-For example, if the array , , so return .
+For example, if the array ar=[1,2,3], 1+2+3 =6 , so return 6.
 
 Function Description
 
@@ -22,5 +22,29 @@ Output Format
 
 Print the sum of the array's elements as a single integer.
 
+Sample input:
+6
+1 2 3 4 10 11
+
+Sample Output
+31
+
 */
 
+let input = `6
+1 2 3 4 10 11`;
+
+function simpleArraySum(ar) {
+    let len = parseInt(ar.split('')[0]);
+    let array = ar.split('\n')[1].split(' ');
+    let sum = 0;
+    for (let i = 0; i<array.length; i++) {
+        sum += parseInt(array[i])
+    }
+
+    return sum;
+
+
+}
+
+console.log(simpleArraySum(input))
